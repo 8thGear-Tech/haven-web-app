3c0929c35940c78e3332a7be0eee47b7c55567a7
Awelewa165 <oluwatoyinawelewa7@gmail.com>
2022-12-22 15:07:37 +0100

    button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import * as React from "react";
 // import { Component } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-
+import { Buttons } from "../src/pages/websitePages/allActivities/allActivities";
 //external import
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>{/* <Route path="/" element={<RegisterNowBtn />} /> */}</Routes>
+      <Routes>
+        {" "}
+        <Route path="/" element={<Buttons />} />{" "}
+      </Routes>
     </BrowserRouter>
   );
 }
