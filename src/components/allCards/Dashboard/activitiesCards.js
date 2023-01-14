6d817ac78989ad81ab17fa8461d9125c5e0f90bb
chris_echelon <christopherofoegbu89@gmail.com>
2023-01-14 12:08:49 +0100

    recent

--- src/components/allCards/Dashboard/activitiesCards.js
+++ src/components/allCards/Dashboard/activitiesCards.js
@@ -1,2 +1,35 @@
 // MaterialsandActivitiesCard
 // ActivitiesPerfomanceCard
+
+// const SinglePiggeryTrainingCards = (props) => {
+//   const { image, id } = props;
+//   return (
+//     <>
+//       <img
+//         className="img-fluid w-100"
+//         // style={{ width: 358.8 }}
+//         src={image}
+//         alt={id}
+//       />
+//     </>
+//   );
+// };
+
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+export function MaterialsandActivitiesCard() {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+}
