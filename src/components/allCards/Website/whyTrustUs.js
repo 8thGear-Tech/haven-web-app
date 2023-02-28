bad980349146b48e5d2b1ad1026950b501e9a62b
opeyemibioku <opeyemibioku@gmail.com>
2023-02-28 16:42:59 +0100

    merged with cardComponent branch

--- src/components/allCards/Website/whyTrustUs.js
+++ src/components/allCards/Website/whyTrustUs.js
@@ -20,10 +20,6 @@ export const WhyTrustUsCard = (props) => {
           <h4 className="mt-1">{titleText}</h4>
           <Card.Text className="">{paragraphText}</Card.Text>
         </Card.Body>
-        <Card.Body className="text-center">
-          <h4 className="mt-1">{titleText}</h4>
-          <Card.Text className="">{paragraphText}</Card.Text>
-        </Card.Body>
       </div>
     </>
   );
