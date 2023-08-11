85e895080cd8c97ef86f282e1b0558e2421de29a
lilyflowr <ibadiela@gmail.com>
2023-08-11 15:48:38 +0100

    calender

--- src/pages/dashboardPages/champ/champCalenderPage.js
+++ src/pages/dashboardPages/champ/champCalenderPage.js
@@ -1,125 +1,130 @@
-import {
-  ChampNavbar,
-  // ChampSideNavBar,
-} from "../../../components/Navbar/champNavbar";
+import { ChampNavbar } from "../../../components/Navbar/champNavbar";
 import React, { useState } from "react";
 import { Container, Row, Col, Button, Modal } from "react-bootstrap";
-// import moment from "moment";
+import moment from "moment";
 import Card from "react-bootstrap/Card";
 import havenlogo from "../../../assets/icons/havenlogo.png";
+
 const Champcalendar = () => {
   return (
     <>
       <ChampNavbar />
-      <div className="container-fluid">
-        <div className="row d-flex justify-content-center">
-          <div className="col-lg-3">
-            {/* <ChampSideNavBar /> */}
+
+      {/* Material rectangle */}
+      <div style={{ marginTop: '150px' }}>
+        <Container>
+          <div className="materialRectangle"
+            style={{ width: '100%', height: '100%', 
+            background: 'white', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)', 
+            borderRadius: 10, border: '0.50px black solid', 
+            display: 'flex', alignItems: 'center', 
+            justifyContent: 'space-between', padding: '0 20px' }}>
+
+            <div>Calendar</div>
+            <div style={{ display: 'flex', 
+            alignItems: 'center' }}>
+
+              <div>Home</div>
+              <div style={{ marginLeft: '10px', 
+              transform: 'rotate(-90deg)' }}>⤒</div>
+            </div>
           </div>
-          <div className="col-lg-9 col-md-12">
+        </Container>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row d-flex justify-content-center"> 
+          <div className="col-lg-12 col-md-12">
+          
             <div className="row">
-              {/* <div className="col-6">
+              <div className="col-12 col-md-6">
+              
                 <Calendar />
-              </div> */}
-              <div className="col-6">
+              </div>
+              <div className="col-12 col-md-6">
                 <Timestamp />
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-4">
-            <ChampSideNavBar />
-          </div>
-          <div className="col-lg-8">
-            <div className="row">
-              <div className="col-6">  </div>
-              <div className="col-6">
-                <Timestamp/>
-            </div>
-            </div>
-
-            
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
-// const Calendar = () => {
-//   const [selectedDate, setSelectedDate] = useState(moment());
-//   const [showModal, setShowModal] = useState(false);
 
-//   const daysInMonth = selectedDate.daysInMonth();
-//   const firstDayOfMonth = moment(selectedDate).startOf("month").day();
+const Calendar = () => {
+  const [selectedDate, setSelectedDate] = useState(moment());
+  const [showModal, setShowModal] = useState(false);
 
-//   const handlePrevMonth = () => {
-//     setSelectedDate(moment(selectedDate).subtract(1, "month"));
-//   };
+  const daysInMonth = selectedDate.daysInMonth();
+  const firstDayOfMonth = moment(selectedDate).startOf("month").day();
 
-//   const handleNextMonth = () => {
-//     setSelectedDate(moment(selectedDate).add(1, "month"));
-//   };
+  const handlePrevMonth = () => {
+    setSelectedDate(moment(selectedDate).subtract(1, "month"));
+  };
 
-//   const handleDayClick = (day) => {
-//     setSelectedDate(moment(selectedDate).date(day));
-//     setShowModal(true);
-//   };
+  const handleNextMonth = () => {
+    setSelectedDate(moment(selectedDate).add(1, "month"));
+  };
 
-//   return (
-//     <Container className="shadow-lg p-3 mb-5 bg-white rounded">
-//       <Row>
-//         <Col>
-//           <Button onClick={handlePrevMonth}>Prev</Button>
-//         </Col>
-//         <Col>{selectedDate.format("MMMM YYYY")}</Col>
-//         <Col>
-//           <Button onClick={handleNextMonth}>Next</Button>
-//         </Col>
-//       </Row>
-//       <Row>
-//         {[0, 1, 2, 3, 4, 5, 6].map((day) => (
-//           <Col key={day}>{moment().day(day).format("ddd")}</Col>
-//         ))}
-//       </Row>
-//       {[...Array(Math.ceil((daysInMonth + firstDayOfMonth) / 7)).keys()].map(
-//         (week) => (
-//           <Row key={week}>
-//             {[...Array(7).keys()].map((day) => {
-//               const dayNumber = week * 7 + day + 1 - firstDayOfMonth;
-//               if (dayNumber <= 0 || dayNumber > daysInMonth) {
-//                 return <Col key={day}></Col>;
-//               }
-//               return (
-//                 <Col
-//                   key={day}
-//                   className="day"
-//                   onClick={() => handleDayClick(dayNumber)}
-//                 >
-//                   {dayNumber}
-//                 </Col>
-//               );
-//             })}
-//           </Row>
-//         )
-//       )}
-//       <Modal show={showModal} onHide={() => setShowModal(false)}>
-//         <Modal.Header closeButton>
-//           <Modal.Title>{selectedDate.format("MMMM Do YYYY")}</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           This is where you could show more information about the selected day.
-//         </Modal.Body>
-//       </Modal>
-//     </Container>
-//   );
-// };
+  const handleDayClick = (day) => {
+    setSelectedDate(moment(selectedDate).date(day));
+    setShowModal(true);
+  };
+
+  return (
+    <Container className="shadow-lg p-3 mb-5 bg-white rounded">
+      <h2 className=" text-center">Calender</h2>
+      <Row className="m-5 ">
+        <Col className="m-5 ">
+          <Button onClick={handlePrevMonth}>Prev</Button>
+        </Col>
+        <Col className="m-5 ">{selectedDate.format("MMMM YYYY")}</Col>
+        <Col className="m-5 ">
+          <Button onClick={handleNextMonth}>Next</Button>
+        </Col>
+      </Row>
+      <Row className="m-5 ">
+        {[0, 1, 2, 3, 4, 5, 6].map((day) => (
+          <Col key={day}>{moment().day(day).format("ddd")}</Col>
+        ))}
+      </Row>
+      {[...Array(Math.ceil((daysInMonth + firstDayOfMonth) / 7)).keys()].map(
+        (week) => (
+          <Row className="m-5 " key={week}>
+            {[...Array(7).keys()].map((day) => {
+              const dayNumber = week * 7 + day + 1 - firstDayOfMonth;
+              if (dayNumber <= 0 || dayNumber > daysInMonth) {
+                return <Col key={day}></Col>;
+              }
+              return (
+                <Col className="day "
+                  key={day}
+                  
+                  onClick={() => handleDayClick(dayNumber)}
+                >
+                  {dayNumber}
+                </Col>
+              );
+            })}
+          </Row>
+        )
+      )}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{selectedDate.format("MMMM Do YYYY")}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This is where you could show more information about the selected day.
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+};
 const Timestamp = () => {
   return (
     <>
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "18rem", height: "100%" }}>
         <Card.Img variant="top" src={havenlogo} />
         <Card.Body style={{ fill: "Solid #e2e9f0 " }}>
           <Card.Link href="#">About </Card.Link>
